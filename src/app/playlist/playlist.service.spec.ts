6e1de14f645467cb7967f39b7b51e3a0185aea37
nvargas <nicolasvargas33@gmail.com>
2018-12-06 08:08:37 +0100

    WIP Added comments and started playlist service test coverage

--- src/app/playlist/playlist.service.spec.ts
+++ src/app/playlist/playlist.service.spec.ts
@@ -2,27 +2,56 @@ import { TestBed } from '@angular/core/testing';
 
 import { PlaylistService } from './playlist.service';
 import {
-  HttpClientTestingModule
+  HttpClientTestingModule, HttpTestingController
 } from '@angular/common/http/testing';
+import { Playlist } from './playlist';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
+import { PlaylistResult } from './playlist-result';
 
 describe('PlaylistServiceService', () => {
 
-  let httpClientSpy: { get: jasmine.Spy };
-  let playlistService: PlaylistService;
-
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [HttpClientTestingModule],
-    providers: [PlaylistService]
-  }));
+  let httpTestingController: HttpTestingController;
+  let service: PlaylistService;
 
   beforeEach(() => {
-    // TODO: spy on other methods too
-    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
-    playlistService = new PlaylistService(<any>httpClientSpy);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PlaylistService]
+    });
+
+    service = TestBed.get(PlaylistService);
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
-    const service: PlaylistService = TestBed.get(PlaylistService);
-    expect(service).toBeTruthy();
+    const playlistService: PlaylistService = TestBed.get(PlaylistService);
+    expect(playlistService).toBeTruthy();
+  });
+
+  it('should return an observable of playlists', () => {
+    const playlist1: Playlist = new Playlist(1, 'p1', 'psm', 'pmd', 'playlist1', 'tracks', 1);
+    const playlist2: Playlist = new Playlist(2, 'p1', 'psm', 'pmd', 'playlist2', 'tracks', 4);
+    const expectedPlaylists: Playlist[] = [playlist1, playlist2];
+    const playlistResult: PlaylistResult = {
+      data: expectedPlaylists,
+      next: null,
+      total: expectedPlaylists.length
+    };
+
+
+    service.getPlaylists().subscribe(
+      list => expect(list).toEqual(expectedPlaylists)
+    );
+
+    const req = httpTestingController.expectOne('/api/user/5/playlists');
+
+    req.flush(playlistResult);
+
   });
 });
