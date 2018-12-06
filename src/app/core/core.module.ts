5f3d876aa2ab42d3015f9b4a83842c360a909708
Nicolas Vargas <n.vargas@lectra.com>
2018-12-06 17:22:41 +0100

    Added tests

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ServicesModule } from './services/services.module';
+import { environment } from './../../environments/environment';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { CorsInterceptor } from './corsInterceptor';
+
+let devProviders = [{
+  provide: HTTP_INTERCEPTORS,
+  useClass: CorsInterceptor,
+  multi: true
+}];
+if(environment.production) {
+  devProviders = [];
+}
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     ServicesModule
-  ]
+  ],
+  providers: [...devProviders]
 })
 export class CoreModule { }
