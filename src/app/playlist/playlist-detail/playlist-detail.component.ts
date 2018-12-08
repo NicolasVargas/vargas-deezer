43932f407fe114112260241c89370dfbc275fcc2
Nicolas Vargas <nicolasvargas33@gmail.com>
2018-12-08 17:52:37 +0100

    Fetching playlist tracks and setting MatTabeDataSource or futur pagination and filtering

--- src/app/playlist/playlist-detail/playlist-detail.component.ts
+++ src/app/playlist/playlist-detail/playlist-detail.component.ts
@@ -1,33 +1,55 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, ParamMap, Router } from '@angular/router';
-import { switchMap, first } from 'rxjs/operators';
-import { PlaylistService } from '../playlist.service';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { Playlist } from '../playlist';
+import { PlaylistService } from '../playlist.service';
+import { Track } from '../track';
+import { TrackResult } from '../track-result';
 
 @Component({
   selector: 'app-playlist-detail',
   templateUrl: './playlist-detail.component.html',
   styleUrls: ['./playlist-detail.component.scss']
 })
-export class PlaylistDetailComponent implements OnInit {
+export class PlaylistDetailComponent implements OnInit, OnDestroy {
   playlist: Playlist;
+  tracks: TrackResult;
+  destroySubject: Subject<boolean> = new Subject<boolean>();
   columnsToDisplay = ['index', 'title', 'duration', 'artist'];
+  tracksResult: Observable<TrackResult>;
+  tracksDataSource = new MatTableDataSource<Track>([]);
+
   constructor(
     private route: ActivatedRoute,
-    private router: Router,
     private playlistService: PlaylistService
   ) { }
 
   ngOnInit() {
     this.route.paramMap
-      .pipe(
-        switchMap((params: ParamMap) =>
-          this.playlistService.getPlaylist((+params.get('id')))
-        ),
-        first()
-      )
-      .subscribe(data => this.playlist = data);
+      .pipe(takeUntil(this.destroySubject))
+      .subscribe((params: ParamMap) => {
+        let id: number = +params.get('id');
+
+        // Fetch playlist details
+        this.playlistService.getPlaylist(id)
+          .pipe(takeUntil(this.destroySubject))
+          .subscribe((playlist: Playlist) => {
+            this.playlist = playlist;
+          });
+
+        // Fetch playlist tracks
+        this.playlistService.getPlaylistTracks(id)
+          .pipe(takeUntil(this.destroySubject))
+          .subscribe((tracks: TrackResult) => {
+            this.tracksDataSource.data = tracks.data;
+          })
+      })
   }
 
+  ngOnDestroy() {
+    this.destroySubject.next(true);
+    this.destroySubject.unsubscribe();
+  }
 }
--- src/app/playlist/playlist.service.ts
+++ src/app/playlist/playlist.service.ts
@@ -1,43 +1,45 @@
-import { Injectable } from '@angular/core';
-import { Observable, BehaviorSubject } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
-import { PlaylistResult } from './playlist-result';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import { Playlist } from './playlist';
+import { PlaylistResult } from './playlist-result';
 import { TrackResult } from './track-result';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlaylistService {
-  private _playlistResult: BehaviorSubject<PlaylistResult>;
-  private _tracksResult: BehaviorSubject<TrackResult>;
+  private playlistResult: BehaviorSubject<PlaylistResult>;
+  private tracksResult: BehaviorSubject<TrackResult>;
 
   constructor(private http: HttpClient) {
-    this._playlistResult = new BehaviorSubject<PlaylistResult>(null);
+    this.playlistResult = new BehaviorSubject<PlaylistResult>(null);
+    this.tracksResult = new BehaviorSubject<TrackResult>(null);
   }
 
   /**
    * Fetch a brand new playlists page
    */
   getPlaylists(userId: Number = 5): Observable<PlaylistResult> {
-    return this.http.get<PlaylistResult>(`https://api.deezer.com/user/${userId}/playlists`)
+    return this.http.get<PlaylistResult>(`${environment.apiUrl}/user/${userId}/playlists`)
       .pipe(switchMap((playlistResult: PlaylistResult) => {
-        this._playlistResult.next(playlistResult);
-        return this._playlistResult.asObservable();
+        this.playlistResult.next(playlistResult);
+        return this.playlistResult.asObservable();
       }));
   }
 
   hasMorePlaylists(): boolean {
-    return !!this._playlistResult.getValue() && (this._playlistResult.getValue().next != null);
+    return !!this.playlistResult.getValue() && (this.playlistResult.getValue().next != null);
   }
 
   loadMorePlaylists(): Observable<PlaylistResult> {
     if (this.hasMorePlaylists()) {
-      const nextObservable = this.http.get<PlaylistResult>(this._playlistResult.getValue().next);
+      const nextObservable = this.http.get<PlaylistResult>(this.playlistResult.getValue().next);
       const subscription = nextObservable.subscribe((newResult: PlaylistResult) => {
-        newResult.data = this._playlistResult.getValue().data.concat(newResult.data);
-        this._playlistResult.next(newResult);
+        newResult.data = this.playlistResult.getValue().data.concat(newResult.data);
+        this.playlistResult.next(newResult);
 
         subscription.unsubscribe();
       });
@@ -46,27 +48,27 @@ export class PlaylistService {
   }
 
   getPlaylist(playlistId: Number = 273953): Observable<Playlist> {
-    return this.http.get<Playlist>(`https://api.deezer.com/playlist/${playlistId}`);
+    return this.http.get<Playlist>(`${environment.apiUrl}/playlist/${playlistId}`);
   }
 
   getPlaylistTracks(playlistId: Number = 273953): Observable<TrackResult> {
-    return this.http.get<TrackResult>(`https://api.deezer.com/playlist/${playlistId}/tracks`)
+    return this.http.get<TrackResult>(`${environment.apiUrl}/playlist/${playlistId}/tracks`)
       .pipe(switchMap((tracksResult: TrackResult) => {
-        this._tracksResult.next(tracksResult);
-        return this._tracksResult.asObservable();
+        this.tracksResult.next(tracksResult);
+        return this.tracksResult.asObservable();
       }));
   }
 
   hasMorePlaylistTracks(): boolean {
-    return !!this._playlistResult.getValue() && (this._playlistResult.getValue().next != null);
+    return !!this.playlistResult.getValue() && (this.playlistResult.getValue().next != null);
   }
 
   loadMorePlaylistTracks(): Observable<TrackResult> {
     if (this.hasMorePlaylistTracks()) {
-      const nextObservable = this.http.get<TrackResult>(this._tracksResult.getValue().next);
+      const nextObservable = this.http.get<TrackResult>(this.tracksResult.getValue().next);
       const subscription = nextObservable.subscribe((newResult: TrackResult) => {
-        newResult.data = this._tracksResult.getValue().data.concat(newResult.data);
-        this._tracksResult.next(newResult);
+        newResult.data = this.tracksResult.getValue().data.concat(newResult.data);
+        this.tracksResult.next(newResult);
 
         subscription.unsubscribe();
       });
